4a84d0221a530b1f8d8bbe849f29bcc300e33ad9
John Hatton <hattonjohn@gmail.com>
2013-06-03 08:30:24 -0700

    Merged in john_thomson/l10nsharp/HttpUtilityNamespace (pull request #2)
    Move HttpUtilityFromMono to L10NSharp namespace to avoid conflict

---
+++ src/SampleApp/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using L10NSharp;
+using SampleApp.Properties;
+
+namespace SampleApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            SetUpLocalization();
+
+            L10NSharp.LocalizationManager.SetUILanguage(Settings.Default.UserInterfaceLanguage, false);
+
+            Application.Run(new Form1());
+            Settings.Default.Save();
+        }
+
+        public static void SetUpLocalization()
+        {
+            var installedStringFileFolder = "../../";
+
+            try
+            {
+                //You sure won't want to use "temp" in your real app. Consider AppData/Product.
+                //Downside of that one is that the user needs write access to that folder.
+                var pathForStoringLocalizations = Path.GetTempPath();
+
+                //if this is your first time running the app, the library will query the OS for the
+                //the default language. If it doesn't have that, it puts up a dialog listing what
+                //it does have to offer.
+
+                var theLanguageYouRememberedFromLastTime = Settings.Default.UserInterfaceLanguage;
+
+                LocalizationManager.Create(theLanguageYouRememberedFromLastTime,
+                                           "SampleApp", "SampleApp", Application.ProductVersion,
+                                           installedStringFileFolder,
+                                           Path.Combine(pathForStoringLocalizations, "Localizations"), 
+                                           Resources.Icon, //replace with your icon
+                                           "sampleappLocalizations@nowhere.com", "SampleApp");
+
+                Settings.Default.UserInterfaceLanguage = LocalizationManager.UILanguageId;
+            }
+            catch (Exception error)
+            {
+                if (Process.GetProcesses().Count(p => p.ProcessName.ToLower().Contains("SampleApp")) > 1)
+                {
+                    MessageBox.Show("There is another copy of SampleApp already running while SampleApp was trying to set up localization.");
+                    Environment.FailFast("SampleApp couldn't set up localization");
+                }
+
+                if (error.Message.Contains("SampleApp.en.tmx"))
+                {
+                    MessageBox.Show("Sorry. SampleApp is trying to set up your machine to use this new version, but something went wrong getting at the file it needs. If you restart your computer, all will be well.");
+
+                    Environment.FailFast("SampleApp couldn't set up localization");
+                }
+
+                //otherwise, we don't know what caused it.
+                throw;
+            }
+        }
+
+    }
+}
