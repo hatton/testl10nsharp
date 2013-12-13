8f00aebff2d45c4845316e479e454e8b8fd11708
Eberhard Beilharz <eb1@sil.org>
2013-12-13 10:43:17 +0100

    Adding .gitattributes and .gitignore files
    This is in preparation for the migration to git.

---
+++ src/L10NSharp/UI/LanguageChoosingDialog.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+using L10NSharp.Translators;
+
+namespace L10NSharp.UI
+{
+    public partial class LanguageChoosingDialog : Form
+    {
+        private readonly CultureInfo _requestedCulture;
+        private string _originalMessageTemplate;
+
+        public LanguageChoosingDialog(CultureInfo requestedCulture, Icon icon)
+        {
+            _requestedCulture = requestedCulture;
+            InitializeComponent();
+            this.Icon = icon;
+            _originalMessageTemplate = _messageLabel.Text;
+            _messageLabel.Text = string.Format(_originalMessageTemplate, requestedCulture.EnglishName, requestedCulture.NativeName);
+            Application.Idle += new EventHandler(Application_Idle);
+        }
+
+        void Application_Idle(object sender, EventArgs e)
+        {
+            Application.Idle -= new EventHandler(Application_Idle);
+            var translator = new BingTranslator("en", _requestedCulture.TwoLetterISOLanguageName);
+            try
+            {
+                var s = translator.TranslateText(string.Format(_originalMessageTemplate, _requestedCulture.EnglishName, _requestedCulture.NativeName));
+                if (!string.IsNullOrEmpty(s))
+                    _messageLabel.Text = s;
+                _OKButton.Text = translator.TranslateText("OK");
+                Text = translator.TranslateText(Text);
+            }
+            catch (Exception)
+            {
+                //swallow
+            }
+        }
+
+        public string SelectedLanguage;
+
+        private void _OKButton_Click(object sender, System.EventArgs e)
+        {
+            Close();
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            SelectedLanguage = uiLanguageComboBox1.SelectedLanguage; 
+            base.OnClosing(e);
+        }
+    }
+}
---
+++ src/SampleApp/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using L10NSharp;
+using SampleApp.Properties;
+
+namespace SampleApp
+{
+    public partial class Form1 : Form
+    {
+        private Label _dynamicLabel;
+
+        public Form1()
+        {
+            InitializeComponent();         
+ 
+         
+        }
+
+
+        private void UpdateDynamicLabel()
+        {
+			if(_dynamicLabel!=null)
+				_dynamicLabel.Text = LocalizationManager.GetDynamicString("SampleApp", "The User Name", Environment.UserName);
+        }
+
+        private void uiLanguageComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Settings.Default.UserInterfaceLanguage = uiLanguageComboBox1.SelectedLanguage;
+            LocalizationManager.SetUILanguage(uiLanguageComboBox1.SelectedLanguage, true);
+            UpdateDynamicLabel();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            LocalizationManager.ShowLocalizationDialogBox(this);
+            UpdateDynamicLabel();
+        }
+
+        //This demonstrates how to handle strings which aren't hard-coded, so can't be discovered
+        //by the runtime code scanner. Instead, we ue this GetDynamicString
+		private void button1_Click(object sender, EventArgs e)
+		{
+			_getDynamicStringButton.Enabled = false;
+
+			_dynamicLabel = new Label()
+			{
+				Location = new Point(_getDynamicStringButton.Location.X + 180, _getDynamicStringButton.Location.Y+10)
+			};
+			uiLanguageComboBox1.SelectedLanguage = Settings.Default.UserInterfaceLanguage;
+			UpdateDynamicLabel();
+			this.Controls.Add(_dynamicLabel);
+		}
+    }
+}
