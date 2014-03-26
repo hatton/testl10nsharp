0246c66500ae814dd22a8cb4f81a98f783f70367
tom_bogle <tom_bogle@sil.org>
2014-03-26 11:19:35 -0400

    Improvements to HowToDistributeDialog to handle case where TMX file hasn't yet been created.

---
+++ src/L10NSharp/UI/HowToDistributeDialog.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace L10NSharp.UI
+{
+    public partial class HowToDistributeDialog : Form
+    {
+        private readonly string _targetTmxFilePath;
+
+        public HowToDistributeDialog(string emailForSubmissions, string targetTmxFilePath)
+        {
+            _targetTmxFilePath = targetTmxFilePath;
+			if (!File.Exists(_targetTmxFilePath))
+	        {
+		        _targetTmxFilePath = Path.GetDirectoryName(_targetTmxFilePath);
+				Debug.Assert(_targetTmxFilePath != null);
+		        if (!Directory.Exists(_targetTmxFilePath))
+			        Directory.CreateDirectory(_targetTmxFilePath);
+	        }
+	        InitializeComponent();
+
+#if MONO
+            //Steve M set these all to false in the Designer.cs, but that makes them all disappear on Windows
+            label1.AutoSize = label2.AutoSize = label3.AutoSize = label4.AutoSize = false;
+#endif
+            _emailLabel.Text=emailForSubmissions;
+        }
+
+        private void OnShowTMXFile(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("explorer.exe", "/select, \"" + _targetTmxFilePath + "\"");
+        }
+
+        private void _emailLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("mailto:" + _emailLabel.Text);
+        }
+
+    }
+}
---
+++ src/SampleApp/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using L10NSharp;
+using SampleApp.Properties;
+
+namespace SampleApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            SetUpLocalization();
+
+            L10NSharp.LocalizationManager.SetUILanguage(Settings.Default.UserInterfaceLanguage, false);
+
+            Application.Run(new Form1());
+            Settings.Default.Save();
+        }
+
+        public static void SetUpLocalization()
+        {
+			//your installer should have a folder where you place the tmx files you're shipping with the program
+			var directoryOfInstalledTmxFiles = "../../LocalizationFilesFromInstaller";
+			
+            try
+            {
+				// By using "null" for the following two things, we get AppData/Product.
+				// Note: non-admin-rights users can't write to that folder.
+				string directoryOfDefaultTmxFile = null;
+				// When a user does some translation, their work goes in this directory.
+				string directoryOfUserModifiedTmxFiles = null;
+
+                //if this is your first time running the app, the library will query the OS for the
+                //the default language. If it doesn't have that, it puts up a dialog listing what
+                //it does have to offer.
+
+                var theLanguageYouRememberedFromLastTime = Settings.Default.UserInterfaceLanguage;
+
+                LocalizationManager.Create(theLanguageYouRememberedFromLastTime,
+                                           "SampleApp", "SampleApp", Application.ProductVersion,
+										   directoryOfInstalledTmxFiles, false, directoryOfDefaultTmxFile,
+                                           directoryOfUserModifiedTmxFiles, 
+                                           Resources.Icon, //replace with your icon
+                                           "sampleappLocalizations@nowhere.com", "SampleApp");
+
+                Settings.Default.UserInterfaceLanguage = LocalizationManager.UILanguageId;
+            }
+            catch (Exception error)
+            {
+                if (Process.GetProcesses().Count(p => p.ProcessName.ToLower().Contains("SampleApp")) > 1)
+                {
+                    MessageBox.Show("There is another copy of SampleApp already running while SampleApp was trying to set up localization.");
+                    Environment.FailFast("SampleApp couldn't set up localization");
+                }
+
+                if (error.Message.Contains("SampleApp.en.tmx"))
+                {
+                    MessageBox.Show("Sorry. SampleApp is trying to set up your machine to use this new version, but something went wrong getting at the file it needs. If you restart your computer, all will be well.");
+
+                    Environment.FailFast("SampleApp couldn't set up localization");
+                }
+
+                //otherwise, we don't know what caused it.
+                throw;
+            }
+        }
+
+    }
+}
