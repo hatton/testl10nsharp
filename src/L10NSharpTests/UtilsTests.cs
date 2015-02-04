6be786f5f01d6e4ae1f1d4cca5ded49af99f359a
hasso_pape <hasso_pape@localhost>
2015-02-04 16:43:02 -0600

    Avoid Linux Init Crash - Overwrite Empty Generated Files
    Robustify unit tests (some weren't building on my Linux box)

--- src/L10NSharpTests/UtilsTests.cs
+++ src/L10NSharpTests/UtilsTests.cs
@@ -1,60 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using L10NSharp;
 using NUnit.Framework;
 
 namespace L10NSharp.Tests
 {
-    [TestFixture]
-    public class UtilsTests
-    {
-        [Test]
-        public void TypeDoesNotHaveUnexpectedProperty()
-        {
-            Assert.That(L10NSharp.UI.Utils.HasProperty(typeof(String), "nonsense"), Is.False);
-        }
+	[TestFixture]
+	public class UtilsTests
+	{
+		[Test]
+		public void TypeDoesNotHaveUnexpectedProperty()
+		{
+			Assert.IsFalse(UI.Utils.HasProperty(typeof(String), "nonsense"));
+		}
 
-        [Test]
-        public void InstanceDoesNotHaveUnexpectedProperty()
-        {
-            Assert.That(L10NSharp.UI.Utils.HasProperty("", "nonsense"), Is.False);
-        }
+		[Test]
+		public void InstanceDoesNotHaveUnexpectedProperty()
+		{
+			Assert.IsFalse(UI.Utils.HasProperty("", "nonsense"));
+		}
 
-        [Test]
-        public void InstanceHasExpectedProperty()
-        {
-            Assert.That(L10NSharp.UI.Utils.HasProperty("", "Length"), Is.True);
-        }
+		[Test]
+		public void InstanceHasExpectedProperty()
+		{
+			Assert.IsTrue(UI.Utils.HasProperty("", "Length"));
+		}
 
-        [Test]
-        public void TypeHasExpectedProperty()
-        {
-            Assert.That(L10NSharp.UI.Utils.HasProperty(typeof(int), "MaxValue"), Is.True);
-        }
+		[Test]
+		public void TypeHasExpectedProperty()
+		{
+			Assert.IsTrue(UI.Utils.HasProperty(typeof(int), "MaxValue"));
+		}
 
-        [Test]
-        public void GetPropertyOnExistingMethod_Works()
-        {
-            Assert.That(UI.Utils.GetProperty("abc", "Length"), Is.EqualTo(3));
-        }
+		[Test]
+		public void GetPropertyOnExistingMethod_Works()
+		{
+			Assert.AreEqual(3, UI.Utils.GetProperty("abc", "Length"));
+		}
 
-        [Test]
-        public void GetPropertyOnMissingMethod_ReturnsNull()
-        {
-            Assert.That(UI.Utils.GetProperty("abc", "nonsence"), Is.Null);
-        }
+		[Test]
+		public void GetPropertyOnMissingMethod_ReturnsNull()
+		{
+			Assert.IsNull(UI.Utils.GetProperty("abc", "nonsence"));
+		}
 
-        /// <summary>
-        /// Special case test, because in one case we had some crashes where we tried to call this missing method.
-        /// </summary>
-        [Test]
-        public void GetShortcutKeysOnToolStripButton_ReturnsNull()
-        {
-            var button = new ToolStripButton();
-            Assert.That(UI.Utils.GetProperty(button, "ShortcutKeys"), Is.Null);
-        }
-    }
+		/// <summary>
+		/// Special case test, because in one case we had some crashes where we tried to call this missing method.
+		/// </summary>
+		[Test]
+		public void GetShortcutKeysOnToolStripButton_ReturnsNull()
+		{
+			var button = new ToolStripButton();
+			Assert.IsNull(UI.Utils.GetProperty(button, "ShortcutKeys"));
+		}
+	}
 }
